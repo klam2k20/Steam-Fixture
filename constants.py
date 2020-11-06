42d5b21d71e2575fe6b19f07f6f81f6ad8584264
Kenny Lam <klam2k17@gmail.com>
2020-11-06 13:06:53 -0500

    split code up into steamfixture and gui, added timer, added stop button

---
+++ constants.py
@@ -0,0 +1,31 @@
+#------------------------------------------------------------------ CONSTANTS & VARIABLES ------------------------------------------------------------------
+COUNTER = 0
+STEAM_SENSOR1 = 6
+STEAM_SENSOR2 = 5
+STEAM_SENSOR3 = 4
+TEMP_PROBE_STEAM = ''
+TEMP_PROBE_SURR = ''
+START_TIME = 0
+THRESHOLD = 10
+TIME_INTERVAL = .5
+df = ''
+STATE = True
+
+#------------------------------------------------------------------ INPUTS ------------------------------------------------------------------
+STEAM_APPLIANCE = ''
+FUNCTION = ''
+FOOD_LOAD = ''
+MONITOR_TIME = 0
+SENSOR_HEIGHT = 0
+INITIAL_WATER_MASS = 0
+FINAL_WATER_MASS = 0
+INITIAL_FOOD_MASS = 0
+FINAL_FOOD_MASS = 0
+
+#------------------------------------------------------------------ OUTPUTS ------------------------------------------------------------------
+STEAM_ACCUMULATION = 0
+STEAM_SENSOR_HUMIDITY = 0
+WATER_LOSS = 0
+
+
+
--- steamSensorFixture.py
+++ steamSensorFixture.py
@@ -7,33 +7,19 @@ import glob
 import smtplib
 import ADS1256
 import RPi.GPIO as GPIO
-import csv
 import math
 import numpy as np
 import pandas as pd
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email import encoders
-
-#------------------------------------------------------------------ CONSTANTS & VARIABLES ------------------------------------------------------------------
-STEAM_SENSOR1 = 6
-STEAM_SENSOR2 = 5
-STEAM_SENSOR3 = 4
-TEMP_PROBE_STEAM = ''
-TEMP_PROBE_SURR = ''
-START_TIME = 0
-THRESHOLD = 10
+import constants
 
 #-------------------------------------------------------------- SENSOR READING FUNCTIONS -------------------------------------------------------------------
 def update_temp_id():
-    global TEMP_PROBE_STEAM, TEMP_PROBE_SURR
     try:
         mypath = '/sys/bus/w1/devices/'
         onlylinks = [f for f in os.listdir(mypath) if os.path.islink(os.path.join(mypath, f))]
         onlylinks.remove('w1_bus_master1')
-        TEMP_PROBE_SURR = str(onlylinks[0])
-        TEMP_PROBE_STEAM = str(onlylinks[1])
+        constants.TEMP_PROBE_SURR = str(onlylinks[0])
+        constants.TEMP_PROBE_STEAM = str(onlylinks[1])
     except IndexError:
         print('Temperature probes are not correctly connected')
 
@@ -55,39 +41,6 @@ def read_temp(id):
         temp_c = float(temp_string) / 1000.0
         return temp_c
 
-
-#-------------------------------------------------------------------- EMAIL FUNCTIONS ----------------------------------------------------------------------
-def email_Send(fileName, EMAIL_RECEIVE):
-    EMAIL_SEND = "rtesting708@gmail.com"
-    EMAIL_PASSWORD = "Poohbear1!"
-    print("Sending email")
-    subject =  fileName
-    msg = MIMEMultipart()
-    msg['From'] =  EMAIL_SEND
-    msg['To'] =  EMAIL_RECEIVE
-    msg['Subject'] = subject
-
-    body = "Raw data from steam sensor test."
-    msg.attach(MIMEText(body, 'plain'))
-
-    attachment = open(fileName, 'rb')
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload((attachment).read())
-    encoders.encode_base64(part)
-    part.add_header(
-        "Content-Disposition",
-        "attachment; filename= " + fileName,
-    )
-    msg.attach(part)
-    text = msg.as_string()
-
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(EMAIL_SEND, EMAIL_PASSWORD)
-    server.sendmail(EMAIL_SEND, EMAIL_RECEIVE, text)
-    server.close()
-    print("Email sent")
-
 #--------------------------------------------------------------------- DIRECTORY FUNCTION -----------------------------------------------------------------------
 def new_Dir():
     DATE = time.ctime().split(' ')
@@ -108,13 +61,13 @@ def new_Dir():
     return counter
         
 #--------------------------------------------------------------------- EXCEL FUNCTION -----------------------------------------------------------------------
-def excel_FileName(counter, SENSOR_HEIGHT):
-    return 'Steam_Fixture_' + str(counter) + '.xlsx'
+def excel_FileName():
+    return 'Steam_Fixture_' + str(constants.COUNTER) + '.xlsx'
 
-def dataframe_to_Excel(counter, df, derivative_df, average_Sensor_Humidity, steam_Accum, FOOD_LOAD, MONITOR_TIME, TIME_INTERVAL, SENSOR_HEIGHT, INITIAL_MASS, FINAL_MASS, STEAM_APPLIANCE, FUNCTION):
-    input_df = input_to_df(df,FOOD_LOAD, MONITOR_TIME, TIME_INTERVAL, SENSOR_HEIGHT, INITIAL_MASS, FINAL_MASS, average_Sensor_Humidity, steam_Accum, STEAM_APPLIANCE, FUNCTION)
-    writer = pd.ExcelWriter(excel_FileName(counter, SENSOR_HEIGHT), engine='xlsxwriter')
-    df.to_excel(writer, sheet_name= 'Raw_Steam_Fixture_Data')
+def dataframe_to_Excel(derivative_df):
+    input_df = input_to_df()
+    writer = pd.ExcelWriter(excel_FileName(), engine='xlsxwriter')
+    constants.df.to_excel(writer, sheet_name= 'Raw_Steam_Fixture_Data')
     input_df.to_excel(writer, sheet_name= 'Procedure_Results_Data')
     derivative_df.to_excel(writer, sheet_name= 'Top Derivative - Time')
     workbook = writer.book
@@ -133,37 +86,15 @@ def to_Humidity(raw):
 def format_best_fit_eq(m,b):
     return 'y = ' + '{0:.2f}'.format(m) + 'x +' + '{0:.2f}'.format(b)
 
-def input_to_df(df, FOOD_LOAD, MONITOR_TIME, TIME_INTERVAL, SENSOR_HEIGHT, INITIAL_MASS, FINAL_MASS, average_Sensor_Humidity, steam_Accum, STEAM_APPLIANCE, FUNCTION):
-    input_dict = {'Steam Appliance':['Function', 'Food Load', 'Cook Time (min)', 'Time Interval (min)', 'Sensor Height (in)', 'Initial Mass (g)', 'Final Mass (g)', 'Water Loss (g)',
-                               'Average Steam Sensor Humidity (%)', 'Steam Accumulation (Count * min)', 'Average Steam Temperature (C)'],
-                    STEAM_APPLIANCE: [FUNCTION, FOOD_LOAD, MONITOR_TIME, TIME_INTERVAL, SENSOR_HEIGHT, INITIAL_MASS, FINAL_MASS, INITIAL_MASS - FINAL_MASS,
-                                         average_Sensor_Humidity, steam_Accum, average_steam_temperature(df)] }
+def input_to_df():
+    input_dict = {'Steam Appliance':['Function', 'Food Load', 'Cook Time (min)', 'Time Interval (min)', 'Sensor Height (in)', 'Initial Water Mass (g)', 'Initial Food Mass (g)',
+                    'Final Water Mass (g)', 'Final Food Mass (g)', 'Water Loss (g)','Average Steam Sensor Humidity (%)', 'Steam Accumulation (Count * min)', 'Average Steam Temperature (C)'],
+                    constants.STEAM_APPLIANCE: [constants.FUNCTION, constants.FOOD_LOAD, constants.MONITOR_TIME, constants.TIME_INTERVAL, constants.SENSOR_HEIGHT, constants.INITIAL_WATER_MASS, 
+                    constants.INITIAL_FOOD_MASS, constants.FINAL_WATER_MASS, constants.FINAL_FOOD_MASS, constants.WATER_LOSS, constants.STEAM_SENSOR_HUMIDITY, constants.STEAM_ACCUMULATION, 
+                    average_steam_temperature()] }
     input_df = pd.DataFrame(input_dict)
     return input_df
 
-def print_top10_derivative(d):
-    sorted_derivative = sorted(d, reverse=True)
-    dict_len = len(sorted_derivative)
-    derivative_df_structure = {'Derivative (Count * min)':[], 'Time (min)':[]}
-    derivative_df = pd.DataFrame(derivative_df_structure)
-    counter = 1
-    if  dict_len < 10:
-        print('\n Top ' + str(dict_len) + ' steam slope spikes: ')
-        for derivative in sorted_derivative:
-            if not math.isnan(derivative):
-                derivative_df_new_row = {'Derivative (Count * min)':derivative, 'Time (min)':d[derivative]}
-                derivative_df = derivative_df.append(derivative_df_new_row, ignore_index = True)
-                print('\n {0}. {1:.2f} (Count * min) @ {2:.2f} min'.format(counter, derivative, d[derivative]))
-                counter += 1
-    else:
-        print('\n Top 10 steam slope spikes: ')
-        for derivative in sorted_derivative[:10]:
-            if not math.isnan(derivative):
-                derivative_df_new_row = {'Derivative (Count * min)':derivative, 'Time (min)':d[derivative]}
-                derivative_df = derivative_df.append(derivative_df_new_row, ignore_index = True)
-                print('\n {0}. {1:.2f} @ {2:.2f} min'.format(counter, derivative, d[derivative]))
-                counter += 1
-    return derivative_df
 #----------------------------------------------------------------- DATAFRAME FUNCTION -------------------------------------------------------------------
 def dataframe_Structure():
     columns = {'Time (min)':[], 'Steam Sensor 1 (Count)':[], 'Humidity 1 (%)':[],'Steam Sensor 2 (Count)':[], 'Humidity 2 (%)':[], 
@@ -171,68 +102,88 @@ def dataframe_Structure():
     df = pd.DataFrame(columns)
     return df
 
-def update_Dataframe(deltaTime, ADC_Value, df):
-    global TEMP_PROBE_STEAM, TEMP_PROBE_SURR, STEAM_SENSOR1, STEAM_SENSOR2, STEAM_SENSOR3, START_TIME
-    analog_Steam_Sensor_1 = ADC_Value[STEAM_SENSOR1]
-    analog_Steam_Sensor_2 = ADC_Value[STEAM_SENSOR2]
-    analog_Steam_Sensor_3 = ADC_Value[STEAM_SENSOR3]
+def update_Dataframe(deltaTime, ADC_Value):
+    print(deltaTime)
+    analog_Steam_Sensor_1 = ADC_Value[constants.STEAM_SENSOR1]
+    analog_Steam_Sensor_2 = ADC_Value[constants.STEAM_SENSOR2]
+    analog_Steam_Sensor_3 = ADC_Value[constants.STEAM_SENSOR3]
     humidity_Steam_Sensor_1 = to_Humidity(analog_Steam_Sensor_1)
     humidity_Steam_Sensor_2 = to_Humidity(analog_Steam_Sensor_2)
     humidity_Steam_Sensor_3 = to_Humidity(analog_Steam_Sensor_3)
-    if ((humidity_Steam_Sensor_1 >= THRESHOLD) | (humidity_Steam_Sensor_2 >= THRESHOLD) | (humidity_Steam_Sensor_3 >= THRESHOLD)) & (START_TIME == 0):
-        START_TIME = deltaTime
-    if START_TIME != 0:
+    if ((humidity_Steam_Sensor_1 >= constants.THRESHOLD) | (humidity_Steam_Sensor_2 >= constants.THRESHOLD) | (humidity_Steam_Sensor_3 >= constants.THRESHOLD)) & (constants.START_TIME == 0):
+        constants.START_TIME = deltaTime
+    if constants.START_TIME != 0:
         new_row = {'Time (min)':deltaTime, 
                 'Steam Sensor 1 (Count)':analog_Steam_Sensor_1, 'Humidity 1 (%)':humidity_Steam_Sensor_1,
                 'Steam Sensor 2 (Count)':analog_Steam_Sensor_2, 'Humidity 2 (%)':humidity_Steam_Sensor_2, 
                 'Steam Sensor 3 (Count)':analog_Steam_Sensor_3, 'Humidity 3 (%)':humidity_Steam_Sensor_3, 
-                'Steam Temp. (C)':read_temp(TEMP_PROBE_STEAM), 'Surrounding Temp. (C)':read_temp(TEMP_PROBE_SURR)}
-        df = df.append(new_row, ignore_index = True)
-        return df
-    return df
+                'Steam Temp. (C)':read_temp(constants.TEMP_PROBE_STEAM), 'Surrounding Temp. (C)':read_temp(constants.TEMP_PROBE_SURR)}
+        constants.df = constants.df.append(new_row, ignore_index = True)
+        return constants.df
+    return constants.df
                     
-def average_Steam_Sensor_Humidity(df):
-    total = df['Humidity 1 (%)'].sum() + df['Humidity 2 (%)'].sum() + df['Humidity 3 (%)'].sum()
-    rows = len(df.index)
+def average_Steam_Sensor_Humidity():
+    total = constants.df['Humidity 1 (%)'].sum() + constants.df['Humidity 2 (%)'].sum() + constants.df['Humidity 3 (%)'].sum()
+    rows = len(constants.df.index)
     return total/(3*rows)
 
-def average_steam_temperature(df):
-    return df['Steam Temp. (C)'].mean()
+def average_steam_temperature():
+    return constants.df['Steam Temp. (C)'].mean()
 
-def average_surrounding_humidity(df):
-    return df['Surrounding Humidity (%)'].mean()
+def average_surrounding_humidity():
+    return constants.df['Surrounding Humidity (%)'].mean()
 
-def average_surrounding_temperature(df):
-    return df['Surrounding Temp. (C)'].mean()
+def average_surrounding_temperature():
+    return constants.df['Surrounding Temp. (C)'].mean()
     
-def steam_Accumulation(df):
-    df['Delta T (min)'] = abs(df['Time (min)'].diff(periods=-1))
-    df['Steam Accumulation (Count * min)'] = (df['Steam Sensor 1 (Count)'] * df['Delta T (min)']) + (df['Steam Sensor 2 (Count)'] * df['Delta T (min)']) + (df['Steam Sensor 3 (Count)'] * df['Delta T (min)'])
-    return df['Steam Accumulation (Count * min)'].sum()
+def steam_Accumulation():
+    constants.df['Delta T (min)'] = abs(constants.df['Time (min)'].diff(periods=-1))
+    constants.df['Steam Accumulation (Count * min)'] = (constants.df['Steam Sensor 1 (Count)'] * constants.df['Delta T (min)']) + (constants.df['Steam Sensor 2 (Count)'] * constants.df['Delta T (min)']) + (constants.df['Steam Sensor 3 (Count)'] * constants.df['Delta T (min)'])
+    return constants.df['Steam Accumulation (Count * min)'].sum()
 
-def dataframe_Empty_Check(df):
-    if df.empty:
+def dataframe_Empty_Check():
+    if constants.df.empty:
         print('Dataframe empty. Steam sensor threshold maybe too high or steam sensors maybe broken')
         sys.exit()
 
+def record_data(window):
+    constants.COUNTER = new_Dir()
+    constants.STATE = True
+    update_temp_id()
+    try:
+        constants.df = dataframe_Structure()
+        ADC = ADS1256.ADS1256()
+        ADC.ADS1256_init()
+        while constants.STATE:
+            ADC_Value = ADC.ADS1256_GetAll()
+            deltaTime = window.count/60
+            constants.df = update_Dataframe(deltaTime, ADC_Value)
+            time.sleep(2)
+        
+        
+    except :
+        GPIO.cleanup()
+        print ("\r\nProgram end     ")
+        exit()
+
 #----------------------------------------------------------------- GRAPH FUNCTION -------------------------------------------------------------------
-def humidity_Graph(df):
-    plt.plot('Time (min)', 'Humidity 1 (%)', data = df, color = 'red')
-    plt.plot('Time (min)', 'Humidity 2 (%)', data = df, color = 'black')
-    plt.plot('Time (min)', 'Humidity 3 (%)', data = df, color = 'blue')
+def humidity_Graph():
+    plt.plot('Time (min)', 'Humidity 1 (%)', data = constants.df, color = 'red')
+    plt.plot('Time (min)', 'Humidity 2 (%)', data = constants.df, color = 'black')
+    plt.plot('Time (min)', 'Humidity 3 (%)', data = constants.df, color = 'blue')
     plt.xlabel('Time (min)')
     plt.ylabel('Humidity (%)')
     plt.title('Time vs. Steam Sensor\'s Humidity')
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-def steam_Accumulation_Graph(df, time_interval, MONITOR_TIME):
-    start_Interval = df.iloc[0]['Time (min)']
-    end_Interval = start_Interval + time_interval
+def steam_Accumulation_Graph():
+    start_Interval = constants.df.iloc[0]['Time (min)']
+    end_Interval = start_Interval + constants.TIME_INTERVAL
     legend = []
     label = []
     derivative_time = dict()
-    while start_Interval < MONITOR_TIME:
-        df2 = df.loc[(df['Time (min)'] >= start_Interval) & (df['Time (min)'] <= end_Interval)]
+    while start_Interval < constants.MONITOR_TIME:
+        df2 = constants.df.loc[(constants.df['Time (min)'] >= start_Interval) & (constants.df['Time (min)'] <= end_Interval)]
         x = df2['Time (min)']
         y = df2['Steam Accumulation (Count * min)']
         m,b = np.polyfit(x,y,1)
@@ -242,107 +193,32 @@ def steam_Accumulation_Graph(df, time_interval, MONITOR_TIME):
         label.append(format_best_fit_eq(m,b))
         derivative_time[m] = start_Interval
         start_Interval = end_Interval
-        end_Interval = start_Interval + time_interval
-    plt.plot('Time (min)', 'Steam Accumulation (Count * min)', 'o', data = df, color = 'red')
+        end_Interval = start_Interval + constants.TIME_INTERVAL
+    plt.plot('Time (min)', 'Steam Accumulation (Count * min)', 'o', data = constants.df, color = 'red')
     plt.xlabel('Time (min)')
     plt.ylabel('Steam Accum. (Count * min)')
     plt.title('Time vs. Steam Accumulation')
     #plt.legend(legend, label,loc='center left', bbox_to_anchor=(1, 0.5))
     return derivative_time
 
-def temperature_Graph(df):
-    plt.plot('Time (min)', 'Steam Temp. (C)', data = df, color = 'red')
-    plt.plot('Time (min)', 'Surrounding Temp. (C)', data = df, color = 'blue')
+def temperature_Graph():
+    plt.plot('Time (min)', 'Steam Temp. (C)', data = constants.df, color = 'red')
+    plt.plot('Time (min)', 'Surrounding Temp. (C)', data = constants.df, color = 'blue')
     plt.xlabel('Time (min)')
     plt.ylabel('Temperature (C)')
     plt.title('Time vs. Steam Temperature vs Surrounding Temperature')
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-def steam_Fixture_Graphs(df,time_interval, MONITOR_TIME):
+def steam_Fixture_Graphs():
     plt.figure(num=None, figsize=(10, 10), dpi=80, facecolor='w', edgecolor='k')
     plt.subplot(311)
-    derivative_time = steam_Accumulation_Graph(df,time_interval,MONITOR_TIME)
+    derivative_time = steam_Accumulation_Graph()
 
     plt.subplot(312)
-    humidity_Graph(df)
+    humidity_Graph()
 
     plt.subplot(313)
-    temperature_Graph(df)
+    temperature_Graph()
 
     plt.tight_layout()
     return derivative_time
-    
-#----------------------------------------------------------------- CHECK INPUT FUNCTIONS -------------------------------------------------------------------
-def check_non_negative(num):
-    return num > 0
-    
-def check_float(num):
-    try:
-        float(num)
-        return True
-    except ValueError:
-        return False
-
-def check_string_input(phrase):
-    while 1:
-        string = input(phrase).strip()
-        if not check_float(string):
-            return string
-        else:
-            print('Input must be a string value.')
-
-def check_float_input(phrase):
-    while 1:
-        num = input(phrase).strip()
-        if check_float(num) and check_non_negative(float(num)):
-            return float(num)
-        elif check_float(num) and not check_non_negative(float(num)):
-            print('Input must be a positive value.')
-        else:
-            print('Input must be a float or integer.')
-
-#----------------------------------------------------------------- MAIN FUNCTION -------------------------------------------------------------------
-def main():
-    counter = new_Dir()
-    update_temp_id()
-    TIME_INTERVAL = .5
-    STEAM_APPLIANCE = input('Steam Appliance: ').strip()
-    #EMAIL_RECEIVE = input('Email:').strip()
-    
-    FUNCTION = input('Function: ').strip()
-    FOOD_LOAD = check_string_input('Food Load: ')
-    MONITOR_TIME = check_float_input('Monitor Time (min): ')
-    SENSOR_HEIGHT = check_float_input('Sensor Height (in): ')
-    INITIAL_MASS = check_float_input('Initial Mass (g): ')
-    
-    try:
-        df = dataframe_Structure()
-        startTime = time.time()
-        deltaTime = 0
-        ADC = ADS1256.ADS1256()
-        ADC.ADS1256_init()
-        while (deltaTime < MONITOR_TIME):
-            ADC_Value = ADC.ADS1256_GetAll()
-            deltaTime = update_Delta_Time(startTime)
-            df = update_Dataframe(deltaTime, ADC_Value, df)
-            time.sleep(2)
-        dataframe_Empty_Check(df)
-        FINAL_MASS = check_float_input('Final Mass (g): ')
-        average_Sensor_Humidity = average_Steam_Sensor_Humidity(df)
-        steam_Accum = steam_Accumulation(df)
-        derivative_time = steam_Fixture_Graphs(df,TIME_INTERVAL, MONITOR_TIME)
-        print('Water Loss (g): {0:.2f}'.format(INITIAL_MASS - FINAL_MASS))
-        print('Steam Accumulation - Steam Sensor Average Humidity: {0:,.2f} - {1:.2f} %'.format(steam_Accum, average_Sensor_Humidity))
-        derivative_df = print_top10_derivative(derivative_time)
-        plt.savefig('Steam_Fixture_Graphs.png')
-        dataframe_to_Excel(counter, df, derivative_df, average_Sensor_Humidity, steam_Accum, FOOD_LOAD, MONITOR_TIME, TIME_INTERVAL, SENSOR_HEIGHT, INITIAL_MASS, FINAL_MASS, STEAM_APPLIANCE, FUNCTION)
-        #email_Send(excel_FileName(counter, SENSOR_HEIGHT), EMAIL_RECEIVE)
-        plt.show()
-    except :
-        GPIO.cleanup()
-        print ("\r\nProgram end     ")
-        exit()
-
-if __name__ == "__main__":
-    main()
-
